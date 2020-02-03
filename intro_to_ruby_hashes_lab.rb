496ca39fb937d1a2284b37069d549bdcca8eccd6
Willis Aybar <willisaybar@gmail.com>
2020-02-03 00:23:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,9 +12,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => 
-	{:pieces => 4}, 
-	{:names => {empty}}
+	monopoly = {:railroads => :pieces => 4, :names => {}
 	}
 end
 
