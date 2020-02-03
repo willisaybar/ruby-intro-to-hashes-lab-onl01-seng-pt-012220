ad49c22386ccd061aeb5b83c1b4823f1dedc9852
Willis Aybar <willisaybar@gmail.com>
2020-02-03 00:20:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,12 @@ def actor
 end
 
 def monopoly
-	monopoly = {railroads: { }}
+	monopoly = {:railroads => { }}
 
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {pieces: 4}, {names: {}}
+	monopoly = {:railroads => {:pieces => 4}, 
 	}
 end
 
