2f0a2fcd9f637a1369af65134dd0cc3a81dc5ccd
Willis Aybar <willisaybar@gmail.com>
2020-02-03 00:39:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 	  :pieces => 4, 
 	  :names => {
 	    :reading_railroad => {},
-	    :pennsylvania_railrod => {}
+	    :pennsylvania_railraod => {}
 	  }, 
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25, 
